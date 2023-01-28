37d99c3d54b1a077f37101eca6c76e6c74d44524
Jhynjhiruu <cub3r12@gmail.com>
2023-01-28 20:47:28 +0000

    delete target stuff

--- rust/target/debug/build/proc-macro2-7c34c1a3e1c49a0a/build_script_build-7c34c1a3e1c49a0a.d
+++
@@ -1,5 +0,0 @@
-/home/jay/Documents/GitHub/bannerparse/rust/target/debug/build/proc-macro2-7c34c1a3e1c49a0a/build_script_build-7c34c1a3e1c49a0a: /home/jay/.cargo/registry/src/github.com-1ecc6299db9ec823/proc-macro2-1.0.50/build.rs
-
-/home/jay/Documents/GitHub/bannerparse/rust/target/debug/build/proc-macro2-7c34c1a3e1c49a0a/build_script_build-7c34c1a3e1c49a0a.d: /home/jay/.cargo/registry/src/github.com-1ecc6299db9ec823/proc-macro2-1.0.50/build.rs
-
-/home/jay/.cargo/registry/src/github.com-1ecc6299db9ec823/proc-macro2-1.0.50/build.rs:
--- rust/target/debug/build/quote-9460a2e73e757d38/build_script_build-9460a2e73e757d38.d
+++
@@ -1,5 +0,0 @@
-/home/jay/Documents/GitHub/bannerparse/rust/target/debug/build/quote-9460a2e73e757d38/build_script_build-9460a2e73e757d38: /home/jay/.cargo/registry/src/github.com-1ecc6299db9ec823/quote-1.0.23/build.rs
-
-/home/jay/Documents/GitHub/bannerparse/rust/target/debug/build/quote-9460a2e73e757d38/build_script_build-9460a2e73e757d38.d: /home/jay/.cargo/registry/src/github.com-1ecc6299db9ec823/quote-1.0.23/build.rs
-
-/home/jay/.cargo/registry/src/github.com-1ecc6299db9ec823/quote-1.0.23/build.rs:
